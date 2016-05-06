9095b5f33549afb4a218f7734a8d8df7e4725d7c
Maya Rosecrance <mrosecrance@pivotal.io>
2016-05-06 13:17:56 -0600

    [Finish #119099379]:
    Signed-off-by: Maya Rosecrance <mrosecrance@pivotal.io>

--- spec/tests/failing_spec.rb
+++ spec/tests/failing_spec.rb
@@ -1,31 +1,32 @@
 require "rails_helper"
 
 describe "failing test" do
+  let(:val) {false}
   it("fails") do
-    expect(true).to eq(false)
+    expect(true).to eq(val)
   end
 
   it("fails2") do
-    expect(true).to eq(false)
+    expect(true).to eq(val)
   end
 
   it("passes") do
-    expect(true).to eq(true)
+    expect(true).to eq val
   end
 
   it("passes again") do
-    expect(true).to eq(true)
+    expect(true).to eq(val)
   end
 
   describe("extra describe block here") do
     it("fails3") do
-      expect(true).to eq(false)
+      expect(true).to eq(val)
     end
   end
 
   context("this is the context") do
     it "still fails" do
-      expect(true).to eq(false)
+      expect(true).to eq(val)
     end
   end
 end
